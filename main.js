933dbe6d534b9d04b0d43bb3997338a79727e4c8
xiaobing <chaixb@lizi.com>
2016-07-13 22:05:08 +0800

    try

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ import Paginator from "./src/paginator"
 import ReactDom from 'react-dom'
 import React from 'react'
 
-ReactDom.render(<Paginator pages={8} current={3}/>, document.querySelector('#main'))
+ReactDom.render(<Paginator pages={20} current={3}/>, document.querySelector('#main'))
 
